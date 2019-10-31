60ebad59a12c8bf942beddcfdedcbb22a4b10b0f
Andrei Ionescu <fusedbloxxer@yahoo.com>
2019-10-31 20:07:20 +0200

    Day 10

--- Software Testing/Recap/src/test/java/database/repository/CustomerRepository.java
+++ Software Testing/Recap/src/test/java/database/repository/CustomerRepository.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
-
+    // Spring webflux mysql reactive - r2dbc client
+    // OK
     @Query(value = "select * from customer where email = ?1", nativeQuery = true)
     Customer findCustomerByEmail(String email);
 }
